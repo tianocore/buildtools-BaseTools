ee371c35bf2c41fa564ec76962749445bcc8a38f
hche10x <hche10x@7335b38e-4728-0410-8992-fb3ffe349368>
2007-06-14 04:57:20 +0000

    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@140 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/Python/DscClassObject.py
+++ Source/Python/DscClassObject.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2007, Intel Corporation
 # All rights reserved. This program and the accompanying materials
 # are licensed and made available under the terms and conditions of the BSD License
-# which accompanies this distribution.  The full text of the license may be found at
+# which accompanies this distribution.    The full text of the license may be found at
 # http://opensource.org/licenses/bsd-license.php
 #
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
@@ -18,120 +18,120 @@ from Identification import *
 from Dictionary import *
 
 class DscObject(object):
-  def __init__(self):
-    object.__init__()
+    def __init__(self):
+        object.__init__()
 
 class DscDefines(DscObject):
-  def __init__(self):
-    self.DefinesDictionary = {
-      #Req
-      TAB_DSC_DEFINES_PLATFORM_NAME             : '',
-      TAB_DSC_DEFINES_PLATFORM_GUID             : '',
-      TAB_DSC_DEFINES_PLATFORM_VERSION          : '',
-      TAB_DSC_DEFINES_DSC_SPECIFICATION         : '',
-      TAB_DSC_DEFINES_OUTPUT_DIRECTORY          : '',
-      TAB_DSC_DEFINES_SUPPORTED_ARCHITECTURES   : [],
-      TAB_DSC_DEFINES_BUILD_TARGETS             : [],
-      TAB_DSC_DEFINES_SKUID_IDENTIFIER          : []
-    }
+    def __init__(self):
+        self.DefinesDictionary = {
+            #Req
+            TAB_DSC_DEFINES_PLATFORM_NAME                         : [''],
+            TAB_DSC_DEFINES_PLATFORM_GUID                         : [''],
+            TAB_DSC_DEFINES_PLATFORM_VERSION                      : [''],
+            TAB_DSC_DEFINES_DSC_SPECIFICATION                     : [''],
+            TAB_DSC_DEFINES_OUTPUT_DIRECTORY                      : [''],
+            TAB_DSC_DEFINES_SUPPORTED_ARCHITECTURES               : [''],
+            TAB_DSC_DEFINES_BUILD_TARGETS                         : [''],
+            TAB_DSC_DEFINES_SKUID_IDENTIFIER                      : ['']
+        }
 
 class DscSkuId(DscObject):
-  def __init__(self):
-    self.SkuId = {}     #{ [skuid : skuname], [skuid : skuname], ...}
+    def __init__(self):
+        self.SkuId = {}         #{ [skuid : skuname], [skuid : skuname], ...}
 
 class DscContents(DscObject):
-  def __init__(self):
-    self.Libraries = []
-    self.Components = []        #[['component name', [lib1, lib2, lib3], [bo1, bo2, bo3]], ...]
-    self.LibraryClasses = []
-    self.PcdsFixedAtBuild = []
-    self.PcdsPatchableInModule = []
-    self.PcdsFeatureFlag = []
-    self.PcdsDynamic = []
-    self.PcdsDynamicEx = []
-    self.BuildOptions = []
+    def __init__(self):
+        self.Libraries = []
+        self.Components = []                #[['component name', [lib1, lib2, lib3], [bo1, bo2, bo3]], ...]
+        self.LibraryClasses = []
+        self.PcdsFixedAtBuild = []
+        self.PcdsPatchableInModule = []
+        self.PcdsFeatureFlag = []
+        self.PcdsDynamic = []
+        self.PcdsDynamicEx = []
+        self.BuildOptions = []
 
 class Dsc(DscObject):
-  def __init__(self, filename = None, isMergeAllArches = False):
-    self.identification = Identification()
-    self.Defines = DscDefines()
-    self.Contents = {}
+    def __init__(self, filename = None, isMergeAllArches = False):
+        self.identification = Identification()
+        self.Defines = DscDefines()
+        self.Contents = {}
 
-    for key in DataType.ARCH_LIST_FULL:
-      self.Contents[key] = DscContents()
+        for key in DataType.ARCH_LIST_FULL:
+            self.Contents[key] = DscContents()
+        
+        self.KeyList = [
+            TAB_LIBRARIES, TAB_LIBRARY_CLASSES, TAB_BUILD_OPTIONS, TAB_PCDS_FIXED_AT_BUILD_NULL, \
+            TAB_PCDS_PATCHABLE_IN_MODULE_NULL, TAB_PCDS_FEATURE_FLAG_NULL, \
+            TAB_PCDS_DYNAMIC_NULL, TAB_PCDS_DYNAMIC_EX_NULL, TAB_COMPONENTS, TAB_BUILD_OPTIONS
+        ]
+        
+        if filename != None:
+            self.LoadDscFile(filename)
+            
+        if isMergeAllArches:
+            self.MergeAllArches()
+        
+    def ParseDsc(self, Lines, Key, KeyField):
+        newKey = SplitModuleType(Key)     
+        if newKey[0].find(TAB_LIBRARY_CLASSES.upper()) != -1:
+            GetLibraryClassesWithModuleType(Lines, Key, KeyField, TAB_COMMENT_SPLIT)
+        elif newKey[0].find(TAB_COMPONENTS.upper()) != -1:
+            GetComponents(Lines, Key, KeyField, TAB_COMMENT_SPLIT)
+        else:
+            GetMultipleValuesOfKeyFromLines(Lines, Key, KeyField, TAB_COMMENT_SPLIT)
     
-    self.KeyList = [
-      TAB_LIBRARIES, TAB_LIBRARY_CLASSES, TAB_BUILD_OPTIONS, TAB_PCDS_FIXED_AT_BUILD_NULL, \
-      TAB_PCDS_PATCHABLE_IN_MODULE_NULL, TAB_PCDS_FEATURE_FLAG_NULL, \
-      TAB_PCDS_DYNAMIC_NULL, TAB_PCDS_DYNAMIC_EX_NULL, TAB_COMPONENTS, TAB_BUILD_OPTIONS
-    ]
-    
-    if filename != None:
-      self.LoadDscFile(filename)
-      
-    if isMergeAllArches:
-      self.MergeAllArches()
-    
-  def ParseDsc(self, Lines, Key, KeyField):
-    newKey = SplitModuleType(Key)   
-    if newKey[0].find(TAB_LIBRARY_CLASSES.upper()) != -1:
-      GetLibraryClassesWithModuleType(Lines, Key, KeyField, TAB_COMMENT_SPLIT)
-    elif newKey[0].find(TAB_COMPONENTS.upper()) != -1:
-      GetComponents(Lines, Key, KeyField, TAB_COMMENT_SPLIT)
-    else:
-      GetMultipleValuesOfKeyFromLines(Lines, Key, KeyField, TAB_COMMENT_SPLIT)
-  
-  def MergeAllArches(self):
-    for key in self.KeyList:
-      for arch in DataType.ARCH_LIST:
-        Command = "self.Contents[arch]." + key + ".extend(" + "self.Contents['" + DataType.TAB_ARCH_COMMON + "']." + key + ")"
-        eval(Command)
-      
-  def LoadDscFile(self, filename):
-    EdkLogger.info('Open Dsc File:' + filename)
-    f = open(filename, 'r').read()
-    sects = f.split('[')
-    for sect in sects:
-      tab = (sect.split(TAB_SECTION_END, 1)[0]).upper()
-      if tab == TAB_INF_DEFINES.upper():
-        GetSingleValueOfKeyFromLines(sect, self.Defines.DefinesDictionary, TAB_COMMENT_SPLIT, TAB_EQUAL_SPLIT, True, TAB_VALUE_SPLIT)
-        continue
-      for arch in DataType.ARCH_LIST_FULL + [DataType.TAB_ARCH_NULL]:
+    def MergeAllArches(self):
         for key in self.KeyList:
-          if arch != DataType.TAB_ARCH_NULL:
-            target = (key + DataType.TAB_SPLIT + arch).upper()
-          else:
-            target = key.upper()
-          if SplitModuleType(tab)[0] == target:
-            if arch != DataType.TAB_ARCH_NULL:
-              Command = 'self.ParseDsc(sect, tab, self.Contents[arch].' + key + ')'
-              eval(Command)
-              continue
-            else:
-              Command = "self.ParseDsc(sect, tab, self.Contents['" + DataType.TAB_ARCH_COMMON + "']." + key + ')'
-              eval(Command)
-              continue
+            for arch in DataType.ARCH_LIST:
+                Command = "self.Contents[arch]." + key + ".extend(" + "self.Contents['" + DataType.TAB_ARCH_COMMON + "']." + key + ")"
+                eval(Command)
+            
+    def LoadDscFile(self, filename):
+        EdkLogger.info('Open Dsc File:' + filename)
+        f = open(filename, 'r').read()
+        sects = f.split('[')
+        for sect in sects:
+            tab = (sect.split(TAB_SECTION_END, 1)[0]).upper()
+            if tab == TAB_INF_DEFINES.upper():
+                GetSingleValueOfKeyFromLines(sect, self.Defines.DefinesDictionary, TAB_COMMENT_SPLIT, TAB_EQUAL_SPLIT, True, TAB_VALUE_SPLIT)
+                continue
+            for arch in DataType.ARCH_LIST_FULL + [DataType.TAB_ARCH_NULL]:
+                for key in self.KeyList:
+                    if arch != DataType.TAB_ARCH_NULL:
+                        target = (key + DataType.TAB_SPLIT + arch).upper()
+                    else:
+                        target = key.upper()
+                    if SplitModuleType(tab)[0] == target:
+                        if arch != DataType.TAB_ARCH_NULL:
+                            Command = 'self.ParseDsc(sect, tab, self.Contents[arch].' + key + ')'
+                            eval(Command)
+                            continue
+                        else:
+                            Command = "self.ParseDsc(sect, tab, self.Contents['" + DataType.TAB_ARCH_COMMON + "']." + key + ')'
+                            eval(Command)
+                            continue
 
-  def showDsc(self):
-    print TAB_SECTION_START + TAB_INF_DEFINES + TAB_SECTION_END
-    printDict(self.Defines.DefinesDictionary)
+    def showDsc(self):
+        print TAB_SECTION_START + TAB_INF_DEFINES + TAB_SECTION_END
+        printDict(self.Defines.DefinesDictionary)
 
-    for key in self.KeyList:
-      for arch in DataType.ARCH_LIST_FULL:
-        Command = "printList(TAB_SECTION_START + '" + \
-                  key + DataType.TAB_SPLIT + arch + \
-                  "' + TAB_SECTION_END, self.Contents[arch]." + key + ')'
-        eval(Command)
-    
+        for key in self.KeyList:
+            for arch in DataType.ARCH_LIST_FULL:
+                Command = "printList(TAB_SECTION_START + '" + \
+                                    key + DataType.TAB_SPLIT + arch + \
+                                    "' + TAB_SECTION_END, self.Contents[arch]." + key + ')'
+                eval(Command)
+        
 if __name__ == '__main__':
-  p = Dsc()
-  directory = 'C:\MyWorkspace\\EdkModulePkg'
-  fileList = []
-  for f in os.listdir(directory):
-    if os.path.splitext(os.path.normcase(f))[1] == '.dsc':
-      fileList.append(os.path.join(directory, os.path.normcase(f)))
-      
-  for f in fileList:
-    p.LoadDscFile(f)
-    p.MergeAllArches()
-    p.showDsc()
+    p = Dsc()
+    directory = 'C:\MyWorkspace\\EdkModulePkg'
+    fileList = []
+    for f in os.listdir(directory):
+        if os.path.splitext(os.path.normcase(f))[1] == '.dsc':
+            fileList.append(os.path.join(directory, os.path.normcase(f)))
+            
+    for f in fileList:
+        p.LoadDscFile(f)
+        p.MergeAllArches()
+        p.showDsc()
