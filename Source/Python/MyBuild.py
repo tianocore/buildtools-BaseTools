5461c7a7e3108f18e129fdfc6e5f3b86dea1c4bc
jwang36 <jwang36@7335b38e-4728-0410-8992-fb3ffe349368>
2007-06-22 07:03:29 +0000

    Beautified the output message and added elapsed time message
    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@175 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/Python/MyBuild.py
+++ Source/Python/MyBuild.py
@@ -25,164 +25,66 @@ from AutoGen import *
 from optparse import OptionParser
 from subprocess import *
 
-VersionNumber = "0.01"
-__version__ = "%prog Version " + VersionNumber
-__copyright__ = "Copyright (c) 2007, Intel Corporation  All rights reserved."
-
-
-# check environment
-
-# setup environment
-
-# call tool_setup.bat / edksetup.bat ?
-
-# be compatibile with Ant build.bat
-
-# target, command options process
-##-a, --arch <ARCHS>
-##      ARCHS is a comma separated list containing one or more of: IA32, X64, IPF or EBC which should be built, overrides target.txt?s TARGET_ARCH
-##-p, --platform PlatformName.dsc
-##      Build the platform specified by the DSC file name argument, over rides the ACTIVE_PLATFORM
-##-m, --module Module.inf
-##      Build the module specified by the INF file name argument.
-##-b, --buildtarget BuildTarget
-##      Build the platform using the BuildTarget, overrides target.txt?s TARGET definition.
-##-t, --tagname Tagname
-##      Build the platform using the Tool chain, Tagname, overrides target.txt?s TOOL_CHAIN_TAG definition.
-##-s, --spawn
-##      If this flag is specified, the first two phases, AutoGen and MAKE are mixed, such that as soon as a module can be built, the build will start, without waiting for AutoGen to complete on remaining modules.  While this option provides feedback that looks fast, due to overhead of the AutoGen function, this option is slower than letting AutoGen complete before starting the MAKE phase.
-##-n #
-##      Build the platform using multi-threaded compiler with # threads, values less than 2 will disable multi-thread builds.  Overrides target.txt?s MULTIPLE_THREAD and MAX_CONCURRENT_THREAD_NUMBER
-##-f, --fdf Filename.fdf
-##      The name of the FDF file to use, over-riding the setting in the DSC file.
-##-k, --msft
-##      Make Option: Generate only NMAKE Makefiles: Makefile
-##-g, --gcc
-##      Make Option: Generate only GMAKE Makefiles: GNUmakefile
-##-l, --all
-##      Make Option: Generate both NMAKE and GMAKE makefiles.
-
-# VFR related options: should not be in this tool
-##-r, --vfr VfrScriptFile
-##      VFR Option: Name of the VFR Script File to use
-##-e, --CreateC [Filename]
-##      VFR Option: [DEFAULT] Create the C File, named Filename, otherwise use the VfrScriptFile name as the C code?s filename, i.e., VfrFilename.c
-##-x, --CreateList [Filename]
-##      VFR Option: Create the text list file, named Filename, otherwise use the VfrScriptFile name as the List file?s filename, i.e., VfrFilename.lst
-##-i, --include [Path]
-##      One or more statements to add to the include path for searching.
-##-o, --outdir [DirName]
-##      VFR Option: Directory Location where the C, Lst and/or IFR files are to be created.
-##-b, --Bin [Filename]
-##      VFR Option: Create a binary IFR HII pack file, named Filename, otherwise use the VfrScriptFile name as the IFR file?s name, i.e., VfrFilename.i
-# 
-
-##-c, --flashheader [Filename]
-##      Flash option: Create a header file, default to FlashMap.h, containing flash device definitions for EDK modules which need those flash information.
-##--version
-##      Print version and copyright of this program and exit
-##-v, --verbose
-##      Turn on verbose output with informational messages printed. This is a count value, so specifying ?vv can be used to increase the verbosity level.
-##-q, --quiet
-##      disable all messages except FATAL ERRORS
-##-d, --debug [#]
-##      Enable debug messages, at level #
-##-h, --help
-##      Print copyright, version and usage of this program and exit code: PASS
-def GetOptions():
-    parser = OptionParser(description=__copyright__,version=__version__)
-
-    parser.add_option("-a", "--arch", dest="arch", default=False, action="store_true",
-                      help="build the source without doing check-out in advance")
-    parser.add_option("-c", "--checkout-only", dest="checkout_only", default=False, action="store_true",
-                      help="check-out source tree only, no build will be issued")
-    parser.add_option("-s", "--dir", dest="dir", default="verify", metavar="DIRECTORY",
-                      help="the root directory for the source tree, default to 'verify'")
-    parser.add_option("-f", "--build-cfg", dest="build_cfg", default="Build.cfg", metavar="FILE",
-                      help="configuration file specifying the build steps, default to Build.cfg")
-    parser.add_option("-e", "--empty-tree", dest="empty_tree", default=False, action="store_true",
-                      help="empty source tree first")
-    parser.add_option("-v", "--verbose", dest="verbose", default=False, action="store_true",
-                      help="build with verbose information")
-    parser.add_option("-d", "--debug", dest="debug", default=False, action="store_true",
-                      help="build with debug information")
-    parser.add_option("-q", "--quiet", dest="quiet", default=False, action="store_true",
-                      help="build with little information")
-    parser.add_option("-l", "--log", dest="log", default=None, metavar="FILE",
-                      help="specify a log file")
-
-    (options, args) = parser.parse_args()
-    return options
-
-# determine build mode (platform build, single module build, launch mode build)
-
-# call parser
-
-# call AutoGen
-
-# call makefile
-
-# timing, result analysis, log, report
-
-# build.exe [options] [targets]
-
 # This acts like the main() function for the script, unless it is 'import'ed into another
 # script.
 if __name__ == '__main__':
-    print "Running Operating System =", sys.platform
+    import time
+    startTime = time.clock()
+
+    print "############################################################"
+    print "Host Platform is", sys.platform
     ewb = WorkspaceBuild()
-    #print ewb.Build.keys()
-
-    myArch = ewb.Build["IA32"].Arch
-    print myArch
-
-    myBuild = ewb.Build["IA32"]
-
     myWorkspace = ewb
     apf = ewb.TargetTxt.TargetTxtDictionary["ACTIVE_PLATFORM"][0]
-    myPlatform = myBuild.PlatformDatabase[os.path.normpath(apf)]
+    print "Active Platform is",apf
+
+    myToolchain = ewb.TargetTxt.TargetTxtDictionary["TOOL_CHAIN_TAG"][0]
+    print "First valid tool chain is", myToolchain
+    #print myToolchain
+
+    myBuildTarget = ewb.TargetTxt.TargetTxtDictionary["TARGET"][0]
+    print "First build target is",myBuildTarget
+    #print myBuildTarget
 
     import glob
     buildmf = ""
     makefile = ""
     filesInCurrentDir = glob.glob(os.getcwd() + '\\*.inf')
     if len(filesInCurrentDir) > 0:
-        #mf = "EdkModulePkg/Application/HelloWorld/HelloWorld.inf"
         buildmf = filesInCurrentDir[0][len(myWorkspace.Workspace.WorkspaceDir)+1:]
-        print buildmf
-
-    myToolchain = ewb.TargetTxt.TargetTxtDictionary["TOOL_CHAIN_TAG"][0]
-    #print myToolchain
-
-    myBuildTarget = ewb.TargetTxt.TargetTxtDictionary["TARGET"][0]
-    #print myBuildTarget
+        print "Module build:",buildmf
 
     myBuildOption = {
         "ENABLE_PCH"        :   False,
         "ENABLE_LOCAL_LIB"  :   True,
     }
 
-    buildAg = None
-    for mf in myBuild.ModuleDatabase:
-        #mf = "MdePkg\\Library\\BaseLib\\BaseLib.inf"
-        #if mf in myPlatform.Modules and mf in myBuild.ModuleDatabase:
-        #print mf
+    for arch in ewb.Build:
+        if not arch.upper() == "IA32":
+            continue
+        myArch = ewb.Build[arch].Arch
+        print "Current target architecture is",myArch
 
-        myModule = myBuild.ModuleDatabase[mf]
+        myBuild = ewb.Build[arch]
+        myPlatform = myBuild.PlatformDatabase[os.path.normpath(apf)]
 
-        ag = AutoGen(myModule, myPlatform, myWorkspace, myArch, myToolchain, myBuildTarget)
-        ag.CreateAutoGenFile()
-        if buildmf == str(myModule):
-            makefile = ag.CreateMakefile()
-            buildAg = ag
-        else:
-            ag.CreateMakefile()
+        buildAg = None
+        for mf in myBuild.ModuleDatabase:
+            myModule = myBuild.ModuleDatabase[mf]
 
-##        for lib in ag.ModuleBuildInfo.DependentLibraryList:
-##            ag = AutoGen(lib, myPlatform, myWorkspace, myArch, myToolchain, myBuildTarget)
-##            ag.CreateAutoGenFile()
-##            ag.CreateMakefile()
+            ag = AutoGen(myModule, myPlatform, myWorkspace, myArch, myToolchain, myBuildTarget)
+            ag.CreateAutoGenFile()
+            if buildmf == str(myModule):
+                makefile = ag.CreateMakefile()
+                buildAg = ag
+            else:
+                ag.CreateMakefile()
 
-    if makefile != "":
-        p = Popen(["nmake", "-f", makefile], env=os.environ, cwd=os.path.dirname(makefile)).communicate()
-    #sts = os.waitpid(p.pid, 0)
+        print "############################################################\n"
+        t = ""
+        if len(sys.argv) > 1:
+            t = sys.argv[1]
+        if makefile != "":
+            p = Popen(["nmake", "/nologo", "-f", makefile, t], env=os.environ, cwd=os.path.dirname(makefile)).communicate()
+
+    print "\n[Finished in %s]" % (time.strftime("%M:%S", time.gmtime(int(time.clock() - startTime))))
