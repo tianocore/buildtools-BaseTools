524509028f102e5e4162a707642f6ce6bc62bede
jjin9 <jjin9@7335b38e-4728-0410-8992-fb3ffe349368>
2007-07-06 02:23:29 +0000

    add support for specified inf in CLI add support for exitcode
    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@278 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/Python/Bld.py
+++ Source/Python/Bld.py
@@ -19,6 +19,7 @@ from subprocess import *
 from TargetTxtClassObject import *
 from EdkIIWorkspaceBuild import *
 from AutoGen import *
+from BuildToolError import *
 
 VersionNumber = "0.01"
 __version__ = "%prog Version " + VersionNumber
@@ -82,33 +83,34 @@ def CheckEnvVariable():
 
     if os.getenv("EDK_TOOLS_PATH") == None:
         print "Please set environment variable: EDK_TOOLS_PATH !\n"
-        exit()
+        return 1
     else:
         EDK_TOOLS_PATH = os.environ["EDK_TOOLS_PATH"]
         print "EDK_TOOLS_PATH = %s" % EDK_TOOLS_PATH
 
     if os.getenv("WORKSPACE") == None:
         print "Please set environment variable: WORKSPACE !\n"
-        exit()
+        return 1
     else:
         WORKSPACE = os.environ["WORKSPACE"]
         print "WORKSPACE = %s" % WORKSPACE
 
     if os.getenv("PATH") == None:
         print "Please set environment variable: PATH !\n"
-        exit()
+        return 1
     else:
         PATH = os.environ["PATH"]
         if sys.platform == "win32":
             #print EDK_TOOLS_PATH + "\Bin\Win32"
             if str(PATH).find(EDK_TOOLS_PATH + "\Bin\Win32") == -1:
                 print "Please execute %s\Bin\Win32\edksetup.bat to set %s\Bin\Win32 in environment variable: PATH!\n" % (EDK_TOOLS_PATH, EDK_TOOLS_PATH)
-                exit()
+                return 1
         if sys.platform == "win64":
             #print EDK_TOOLS_PATH + "\Bin\Win64"
             if str(PATH).find(EDK_TOOLS_PATH + "\Bin\Win64") == -1:
                 print "Please execute %s\Bin\Win32\edksetup.bat to set %s\Bin\Win64 in environment variable: PATH!\n" % (EDK_TOOLS_PATH, EDK_TOOLS_PATH)
-                exit()
+                return 1
+    return 0
 
 
 def MyOptionParser():
@@ -135,180 +137,50 @@ def MyOptionParser():
     (opt, args)=parser.parse_args()
     return (opt, args)
 
-
-if __name__ == '__main__':
-#
-# only for Debug, should be removed in release version
-#
-#    os.chdir("C:\Work\EDK2\MdeModulePkg\Application\HelloWorld")
-#    os.chdir("C:\Work\EDK2\MdeModulePkg")
-#    os.chdir("C:\Work\edk2\MdePkg")
-
-#
-# Record Start Time
-#
-    StartTime = time.time()
-    print time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime())
-#
-# Check environment variable: EDK_TOOLS_PATH, WORKSPACE, PATH
-#
-    CheckEnvVariable()
-
-# be compatibile with Ant build.bat ???
-
-#
-# Parse the options and args
-#
-    (opt, args) = MyOptionParser()
-    
+def Process(ModuleFile, PlatformFile, ewb, Target, ToolChain, Arch, args):
     GenC = set(['GenC']) & set(args)
     GenMake = set(['GenMake']) & set(args)
     t = str(' '.join(args))
 
-#
-# Call Parser
-#
-    ewb = WorkspaceBuild(opt.DSCFILE)  #opt.DSCFILE is relative path plus filename
+    for a in Target:
+        for b in ToolChain:
+            for c in Arch:
+                try:
+                    ModuleAutoGen = AutoGen(ModuleFile, PlatformFile, ewb, str(a), b, str(c))
+                except Exception, e:
+                    print e
+                    return 1
+                if GenC != set([]):
+                    try:
+                        ModuleAutoGen.CreateAutoGenFile()
+                    except Exception, e:
+                        print e
+                        return 1
+                elif GenMake != set([]):
+                    try:
+                        ModuleAutoGen.CreateAutoGenFile()
+                        makefile = ModuleAutoGen.CreateMakefile()
+                    except Exception, e:
+                        print e
+                        return 1
+                else:
+                    try:
+                        ModuleAutoGen.CreateAutoGenFile()
+                        makefile = ModuleAutoGen.CreateMakefile()
+                    except Exception, e:
+                        print e
+                        return 1
+                    if makefile != "":
+                        p = Popen(["nmake", "/nologo", "-f", makefile, t], env=os.environ, cwd=os.path.dirname(makefile))
+                        p.communicate()
+                        if p.returncode != None:
+                            return p.returncode
+                    else:
+                        print "Can find Makefile.\n"
+                        return 1
+    return 0
 
-#
-# Merge the Build Options with Parser's Result
-#
-    if opt.TARGET_ARCH == None:
-        opt.TARGET_ARCH = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET_ARCH]
-        if opt.TARGET_ARCH == ['']:
-            print "TARGET_ARCH is None. Don't What to Build.\n"
-            exit()
-    print "TARGET_ARCH is", " ".join(opt.TARGET_ARCH)
-            
-    if opt.DSCFILE == None:
-        opt.DSCFILE = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_ACTIVE_PLATFORM][0]
-        if opt.DSCFILE == '':
-            print "ACTIVE_PLATFORM is None. Don't What to Build.\n"
-#            exit()
-    print "ACTIVE_PLATFORM is %s" % opt.DSCFILE
-
-    if opt.TARGET == None:
-        opt.TARGET = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET]
-        if opt.TARGET == ['']:
-            print "TARGET is None. Don't What to Build.\n"
-            exit()
-    print "TARGET is", " ".join(opt.TARGET)
-
-    if opt.TOOL_CHAIN_TAG == None:
-        opt.TOOL_CHAIN_TAG = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG]
-        if opt.TOOL_CHAIN_TAG == ['']:
-            print "TOOL_CHAIN_TAG is None. Don't What to Build.\n"
-            exit()
-    print "TOOL_CHAIN_TAG is", " ".join(opt.TOOL_CHAIN_TAG)
-
-    if opt.NUM == None:
-        opt.NUM = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_MAX_CONCURRENT_THREAD_NUMBER][0]
-        if opt.NUM == '':
-            print "NUM is 0. Don't What to Build.\n"
-            exit()
-    print "MAX_CONCURRENT_THREAD_NUMBER is", " ".join(opt.NUM)
-
-    if opt.FDFFILE == None and opt.DSCFILE != '':
-        opt.FDFFILE = ewb.DscDatabase[os.path.normpath(opt.DSCFILE)].Defines.DefinesDictionary[TAB_DSC_DEFINES_FLASH_DEFINITION][0]
-        if opt.FDFFILE == '':
-            pass
-    print "FDF FILE is", " ".join(str(opt.FDFFILE))
-
-    
-#
-# Platform Build or Module Build
-#
-    CurWorkDir = os.getcwd()
-    
-    FileList = glob.glob(CurWorkDir + '\\*.inf')
-    FileNum = len(FileList)
-    if len(FileList) > 0:
-        if len(FileList) >= 2:
-            print "There are %d INF filss in %s.\n" % (FileNum, CurWorkDir)
-            exit()
-        if opt.DSCFILE:
-            ModuleFile = FileList[0][len(ewb.Workspace.WorkspaceDir)+1:]
-            print "Module build:", ModuleFile
-            PlatformFile = str(os.path.normpath(opt.DSCFILE))
-            print os.path.normpath(opt.DSCFILE)
-            print "PlatformFile is:", PlatformFile
-            for Target in opt.TARGET:
-                for ToolChain in opt.TOOL_CHAIN_TAG:
-                    for Arch in opt.TARGET_ARCH:
-                        ModuleAutoGen = AutoGen(ModuleFile, PlatformFile, ewb, str(Target), ToolChain, str(Arch))
-                        if GenC != set([]):
-                            ModuleAutoGen.CreateAutoGenFile()
-                            exit()
-                        elif GenMake != set([]):
-                            makefile = ModuleAutoGen.CreateMakefile()
-                            exit()
-                        else:
-                            ModuleAutoGen.CreateAutoGenFile()
-                            makefile = ModuleAutoGen.CreateMakefile()
-                            if makefile != "":
-                                p = Popen(["nmake", "/nologo", "-f", makefile, t], env=os.environ, cwd=os.path.dirname(makefile)).communicate()
-                            else:
-                                print "Can find Makefile.\n"
-        else:
-            print "ERROR: DON'T KNOW WHAT TO BUILD\n"
-    elif opt.DSCFILE:
-        PlatformFile = os.path.normpath(opt.DSCFILE)
-        print "PlatformFile is:", PlatformFile
-        for Target in opt.TARGET:
-                for ToolChain in opt.TOOL_CHAIN_TAG:
-                    for Arch in opt.TARGET_ARCH:
-                        PlatformAutoGen = AutoGen(None, PlatformFile, ewb, str(Target), ToolChain, str(Arch))
-                        if GenC != set([]):
-                            PlatformAutoGen.CreateAutoGenFile()
-                            exit()
-                        elif GenMake != set([]):
-                            makefile = PlatformAutoGen.CreateMakefile()
-                            exit()
-                        else:
-                            PlatformAutoGen.CreateAutoGenFile()
-                            makefile = PlatformAutoGen.CreateMakefile()
-                            if makefile != "":
-                                p = Popen(["nmake", "/nologo", "-f", makefile, t], env=os.environ, cwd=os.path.dirname(makefile)).communicate()
-                            else:
-                                print "Can find Makefile.\n"
-    else:
-        FileList = glob.glob(CurWorkDir + '\\*.dsc')
-        FileNum = len(FileList)
-        if len(FileList) > 0:
-            if len(FileList) >= 2:
-                print "There are %d DSC files in %s.\n" % (FileNum, CurWorkDir)
-                exit()
-            PlatformFile = FileList[0][len(ewb.Workspace.WorkspaceDir)+1:]
-            print "Platform build:", PlatformFile
-            #
-            # Call Parser Again
-            #
-            ewb = WorkspaceBuild(PlatformFile)
-            
-            for Target in opt.TARGET:
-                for ToolChain in opt.TOOL_CHAIN_TAG:
-                    for Arch in opt.TARGET_ARCH:
-                        PlatformAutoGen = AutoGen(None, PlatformFile, ewb, str(Target), ToolChain, str(Arch))
-                        if GenC != set([]):
-                            PlatformAutoGen.CreateAutoGenFile()
-                            exit()
-                        elif GenMake != set([]):
-                            makefile = PlatformAutoGen.CreateMakefile()
-                            exit()
-                        else:
-                            PlatformAutoGen.CreateAutoGenFile()
-                            makefile = PlatformAutoGen.CreateMakefile()
-                            if makefile != "":
-                                p = Popen(["nmake", "/nologo", "-f", makefile, t], env=os.environ, cwd=os.path.dirname(makefile)).communicate()
-                            else:
-                                print "Can find Makefile.\n"
-
-        else:
-            print "ERROR: DON'T KNOW WHAT TO BUILD\n"
-
-#
-# Record Build Process Time
-#
+def CalculateTime(StartTime):
     print time.strftime("Current time is %a, %d %b %Y %H:%M:%S +0000", time.localtime())
     Hour = 0
     Min = 0
@@ -332,3 +204,140 @@ if __name__ == '__main__':
         print "Totol Run Time is %2d:%2d:0%d" %(Hour, Min, Sec)
     elif Hour > 10 and Min < 10 and Sec > 10:
         print "Totol Run Time is %2d:%2d:0%d" %(Hour, Min, Sec)
+
+if __name__ == '__main__':
+#
+# Record Start Time
+#
+    StartTime = time.time()
+    print time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime())
+#
+# Check environment variable: EDK_TOOLS_PATH, WORKSPACE, PATH
+#
+    StatusCode = CheckEnvVariable()
+    if StatusCode != 0:
+        CalculateTime(StartTime)
+        exit(StatusCode)
+
+# be compatibile with Ant build.bat ???
+
+#
+# Parse the options and args
+#
+    try:
+        (opt, args) = MyOptionParser()
+    except:
+        CalculateTime(StartTime)
+        exit(1)
+#
+# Call Parser
+#
+    try:
+        ewb = WorkspaceBuild(opt.DSCFILE)  #opt.DSCFILE is relative path plus filename
+    except:
+        CalculateTime(StartTime)
+        exit(1)
+#
+# Merge the Build Options with Parser's Result
+#
+    if opt.TARGET_ARCH == None:
+        opt.TARGET_ARCH = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET_ARCH]
+        if opt.TARGET_ARCH == ['']:
+            print "TARGET_ARCH is None. Don't What to Build.\n"
+            CalculateTime(StartTime)
+            exit(1)
+    print "TARGET_ARCH is", " ".join(opt.TARGET_ARCH)
+            
+    if opt.DSCFILE == None:
+        opt.DSCFILE = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_ACTIVE_PLATFORM][0]
+#        if opt.DSCFILE == '':
+#            print "ACTIVE_PLATFORM is None. Don't What to Build.\n"
+#            exit()
+    print "ACTIVE_PLATFORM is %s" % opt.DSCFILE
+
+    if opt.TARGET == None:
+        opt.TARGET = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET]
+        if opt.TARGET == ['']:
+            print "TARGET is None. Don't What to Build.\n"
+            CalculateTime(StartTime)
+            exit(1)
+    print "TARGET is", " ".join(opt.TARGET)
+
+    if opt.TOOL_CHAIN_TAG == None:
+        opt.TOOL_CHAIN_TAG = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG]
+        if opt.TOOL_CHAIN_TAG == ['']:
+            print "TOOL_CHAIN_TAG is None. Don't What to Build.\n"
+            CalculateTime(StartTime)
+            exit(1)
+    print "TOOL_CHAIN_TAG is", " ".join(opt.TOOL_CHAIN_TAG)
+
+    if opt.NUM == None:
+        opt.NUM = ewb.TargetTxt.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_MAX_CONCURRENT_THREAD_NUMBER][0]
+    print "MAX_CONCURRENT_THREAD_NUMBER is", " ".join(opt.NUM)
+
+    if opt.FDFFILE == None and opt.DSCFILE != '':
+        opt.FDFFILE = ewb.DscDatabase[os.path.normpath(opt.DSCFILE)].Defines.DefinesDictionary[TAB_DSC_DEFINES_FLASH_DEFINITION][0]
+        if opt.FDFFILE == '':
+            pass
+    print "FDF FILE is", opt.FDFFILE
+
+    
+#
+# Platform Build or Module Build
+#
+    CurWorkDir = os.getcwd()
+
+    if opt.INFFILE:
+        if opt.DSCFILE:
+            ModuleFile = opt.INFFILE
+            print "MODULE build:", ModuleFile
+            PlatformFile = str(os.path.normpath(opt.DSCFILE))
+            print os.path.normpath(opt.DSCFILE)
+            print "PlatformFile is:", PlatformFile
+            StatusCode = Process(ModuleFile, PlatformFile, ewb, opt.TARGET, opt.TOOL_CHAIN_TAG, opt.TARGET_ARCH, args)
+        else:
+            print "ERROR: DON'T KNOW WHAT TO BUILD\n"
+    elif len(glob.glob(CurWorkDir + '\\*.inf')) > 0:
+        FileList = glob.glob(CurWorkDir + '\\*.inf')
+        FileNum = len(FileList)
+        if FileNum >= 2:
+            print "There are %d INF filss in %s.\n" % (FileNum, CurWorkDir)
+            exit()
+        if opt.DSCFILE:
+            ModuleFile = FileList[0][len(ewb.Workspace.WorkspaceDir)+1:]
+            print "Module build:", ModuleFile
+            PlatformFile = str(os.path.normpath(opt.DSCFILE))
+            print os.path.normpath(opt.DSCFILE)
+            print "PlatformFile is:", PlatformFile
+            StatusCode = Process(ModuleFile, PlatformFile, ewb, opt.TARGET, opt.TOOL_CHAIN_TAG, opt.TARGET_ARCH, args)
+        else:
+            print "ERROR: DON'T KNOW WHAT TO BUILD\n"
+    elif opt.DSCFILE:
+        PlatformFile = os.path.normpath(opt.DSCFILE)
+        print "Platform build:", PlatformFile
+        StatusCode = Process(None, PlatformFile, ewb, opt.TARGET, opt.TOOL_CHAIN_TAG, opt.TARGET_ARCH, args)
+    else:
+        FileList = glob.glob(CurWorkDir + '\\*.dsc')
+        FileNum = len(FileList)
+        if FileNum > 0:
+            if FileNum >= 2:
+                print "There are %d DSC files in %s.\n" % (FileNum, CurWorkDir)
+                exit()
+            PlatformFile = FileList[0][len(ewb.Workspace.WorkspaceDir)+1:]
+            print "Platform build:", PlatformFile
+            #
+            # Call Parser Again
+            #
+            ewb = WorkspaceBuild(PlatformFile)
+            StatusCode = Process(None, PlatformFile, ewb, opt.TARGET, opt.TOOL_CHAIN_TAG, opt.TARGET_ARCH, args)
+
+        else:
+            print "ERROR: DON'T KNOW WHAT TO BUILD\n"
+
+#
+# Record Build Process Time
+#
+    CalculateTime(StartTime)
+
+# To Do: add a judgement for return code
+    exit(StatusCode)
