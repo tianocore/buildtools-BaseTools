5ef18d2025f5facbd993d653444ffb35bb7c6b9c
jjin9 <jjin9@7335b38e-4728-0410-8992-fb3ffe349368>
2007-07-25 01:39:50 +0000

    rewrite the TargetTxtClassObject.py
    1. modify the type of member in TargetTxtDictionary to '', only TARGRT and TARGET_ARCH are []
    2. add a TargetTxtDict(), there a use case for this function in main.
    
    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@387 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/Python/Common/TargetTxtClassObject.py
+++ Source/Python/Common/TargetTxtClassObject.py
@@ -13,52 +13,78 @@
 import os
 import EdkLogger
 import DataType
-from Dictionary import *
 from BuildToolError import *
 
 
 class TargetTxtClassObject(object):
     def __init__(self, filename = None):
         self.TargetTxtDictionary = {
-            DataType.TAB_TAT_DEFINES_ACTIVE_PLATFORM                            : [''],
-            DataType.TAB_TAT_DEFINES_ACTIVE_MODULE                                : [''],
-            DataType.TAB_TAT_DEFINES_TOOL_CHAIN_CONF                            : [''],
-            DataType.TAB_TAT_DEFINES_MULTIPLE_THREAD                            : ['Disable'],
-            DataType.TAB_TAT_DEFINES_MAX_CONCURRENT_THREAD_NUMBER : ['2'],
-            DataType.TAB_TAT_DEFINES_TARGET                                             : [''],
-            DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG                             : [''],
-            DataType.TAB_TAT_DEFINES_TARGET_ARCH                                    : ['']
+            DataType.TAB_TAT_DEFINES_ACTIVE_PLATFORM                            : '',
+            DataType.TAB_TAT_DEFINES_ACTIVE_MODULE                              : '',
+            DataType.TAB_TAT_DEFINES_TOOL_CHAIN_CONF                            : '',
+            DataType.TAB_TAT_DEFINES_MULTIPLE_THREAD                            : '',
+            DataType.TAB_TAT_DEFINES_MAX_CONCURRENT_THREAD_NUMBER               : '',
+            DataType.TAB_TAT_DEFINES_TARGET                                     : [''],
+            DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG                             : '',
+            DataType.TAB_TAT_DEFINES_TARGET_ARCH                                : ['']
         }
         if filename != None:
             self.LoadTargetTxtFile(filename)
 
     def LoadTargetTxtFile(self, filename):
-        #EdkLogger.info('LoadTargetTxtFile() Start')
         if os.path.exists(filename) and os.path.isfile(filename):
-            ConvertTextFileToDictionary(filename, self.TargetTxtDictionary, '#', '=', True, None)
-            if self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_ACTIVE_PLATFORM] == []:
-                self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_ACTIVE_PLATFORM]                        = ['']
-            else:
-                self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_ACTIVE_PLATFORM]                        = [self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_ACTIVE_PLATFORM][0]]
-            self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TOOL_CHAIN_CONF]                            = [self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TOOL_CHAIN_CONF][0]]
-            self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_MULTIPLE_THREAD]                            = [self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_MULTIPLE_THREAD][0]]
-            self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_MAX_CONCURRENT_THREAD_NUMBER] = [self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_MAX_CONCURRENT_THREAD_NUMBER][0]]
-            self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET]                                             = list(set(self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET]))
-            self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG]                             = list(set(self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG]))
-            self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET_ARCH]                                    = list(set(self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET_ARCH]))
-            if self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET] == []:
-                self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET] = ['']
-            if self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG] == []:
-                self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG] = ['']
-            if self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET_ARCH] == []:
-                self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET_ARCH] = ['']
-            self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET].sort()
-            self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG].sort()
-            self.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET_ARCH].sort()
-            
-            #EdkLogger.info('LoadTargetTxtFile() End')
+             return self.ConvertTextFileToDictionary(filename, '#', '=')
         else:
             raise ParseError('LoadTargetTxtFile() : No Target.txt file exist')
 
+#
+# Convert a text file to a dictionary
+#
+    def ConvertTextFileToDictionary(self, FileName, CommentCharacter, KeySplitCharacter):
+        """Convert a text file to a dictionary of (name:value) pairs."""
+        try:
+            f = open(FileName,'r')
+        except:
+            EdkLogger.info('Open file failed')
+            return False
+        for Line in f:
+            if Line.startswith(CommentCharacter) or Line.strip() == '':
+                continue
+            LineList = Line.split(KeySplitCharacter,1)
+            if len(LineList) >= 2:
+                Key = LineList[0].strip()
+                if Key.startswith(CommentCharacter) == False and Key in self.TargetTxtDictionary.keys():
+                    if Key == DataType.TAB_TAT_DEFINES_ACTIVE_PLATFORM or Key == DataType.TAB_TAT_DEFINES_TOOL_CHAIN_CONF \
+                      or Key == DataType.TAB_TAT_DEFINES_MULTIPLE_THREAD or Key == DataType.TAB_TAT_DEFINES_MAX_CONCURRENT_THREAD_NUMBER \
+                      or Key == DataType.TAB_TAT_DEFINES_TOOL_CHAIN_TAG or Key == DataType.TAB_TAT_DEFINES_ACTIVE_MODULE:
+                        self.TargetTxtDictionary[Key] = LineList[1].replace('\\', '/').strip()
+                    elif Key == DataType.TAB_TAT_DEFINES_TARGET or Key == DataType.TAB_TAT_DEFINES_TARGET_ARCH:
+                        self.TargetTxtDictionary[Key] = LineList[1].split()
+        f.close()
+
+    def printDict(dict):
+        if dict != None:
+            KeyList = dict.keys()
+            for Key in KeyList:
+                if dict[Key] != '':
+                    print Key + ' = ' + str(dict[Key])
+
+    def printList(key, list):
+        if type(list) == type([]):
+            if len(list) > 0:
+                if key.find(TAB_SPLIT) != -1:
+                    print "\n" + key
+                    for i in list:
+                        print i
+
+def TargetTxtDict(WorkSpace):
+    Target = TargetTxtClassObject()
+    Target.LoadTargetTxtFile(WorkSpace + '\\Conf\\target.txt')
+    return Target
+
+
 if __name__ == '__main__':
     pass
+    Target = TargetTxtDict(os.getenv("WORKSPACE"))
+    print Target.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_MAX_CONCURRENT_THREAD_NUMBER]
+    print Target.TargetTxtDictionary[DataType.TAB_TAT_DEFINES_TARGET]
