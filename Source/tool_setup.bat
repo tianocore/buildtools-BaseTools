c47abe8930c6ee96ce4883722ef83a6a1d5fc0ab
lhauch <lhauch@7335b38e-4728-0410-8992-fb3ffe349368>
2007-08-31 22:40:24 +0000

    Modified for here, with current BaseTools files
    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@672 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/tool_setup.bat
+++ Source/tool_setup.bat
@@ -14,19 +14,7 @@
 REM ##############################################################
 REM # You should not have to modify anything below this line
 REM #
-
-REM
-REM check the EDK_TOOLS_PATH
-REM
-:check_tools_path
-if not defined EDK_TOOLS_PATH goto no_tools_path
-if not exist %EDK_TOOLS_PATH% goto no_tools_path
-
-:path_ok
-pushd .
-cd %~dp0
-set BASE_TOOLS_PATH=%CD%
-popd
+REM # This script executes from the Source Directory of the BaseTools project
 
 if /I "%1"=="-h" goto Usage
 if /I "%1"=="-help" goto Usage
@@ -34,47 +22,46 @@ if /I "%1"=="--help" goto Usage
 if /I "%1"=="/h" goto Usage
 if /I "%1"=="/?" goto Usage
 if /I "%1"=="/help" goto Usage
-if /I "%1"=="build" goto build
-if /I "%1"=="rebuild" goto rebuild
 if NOT "%1"=="" goto Usage
 
+
+REM
+REM check the EDK_TOOLS_PATH
+REM
+if not defined EDK_TOOLS_PATH goto no_tools_path
+if not exist %EDK_TOOLS_PATH% goto no_tools_path
+
+set BASE_TOOLS_PATH=%CD%\..
 set PATH=%BASE_TOOLS_PATH%\Bin;%BASE_TOOLS_PATH%\Bin\Win32;%PATH%
 
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\antlr.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\CompressDll.dll" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\CreateMtFile.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\dlg.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\EfiCompress.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\EfiRom.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\FlashMap.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\FwImage.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenAcpiTable.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenBsfFixup.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenBsfImage.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenCapsuleHdr.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenCRC32Section.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenDepex.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenFfsFile.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenFvImage.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenFvMap.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenSection.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenTEImage.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GuidChk.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\MakeDeps.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\ModifyInf.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\PeiRebase_Ia32.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\PeiRebase_Ipf.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\PeiRebase_X64.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\SecApResetVectorFixup.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\SecFixup.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\SetStamp.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\SplitFile.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\StrGather.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\Strip.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\VfrCompile.exe" goto build
-IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\ZeroDebugData.exe" goto build
+if /I "%1"=="build" goto build
+if /I "%1"=="rebuild" goto rebuild
 
-:skipbuild
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\antlr.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\dlg.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\BootSectImage.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\EfiLdrImage.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\EfiRom.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenBootSector.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenFfs.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenFv.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenFw.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenPage.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenSec.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenVtf.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\Split.exe" goto build
+IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\TianoCompress.exe" goto build
+REM IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\VfrCompile.exe" goto build
+
+REM
+REM Python Programs
+REM
+
+REM IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\build.exe" goto build
+REM IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\GenFds.exe" goto build_python
+REM IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\PcdSyntaxUpdate.exe" goto build_python
+REM IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\TargetTool.exe" goto build_python
+REM IF NOT EXIST "%EDK_TOOLS_PATH%\Bin\Win32\Trim.exe" goto build_python
 goto end
 
 :rebuild
@@ -89,13 +76,22 @@ REM Start to build the Framework Tools
 REM
 
 echo.
-echo Building the Framework Tools
+echo Building the Framework C-based Tools
 echo.
 
 pushd .
 cd %BASE_TOOLS_PATH%\Source\C
 call nmake
 popd
+
+REM :build_python
+REM
+REM pushd .
+REM %BASE_TOOLS_PATH%\Source\Python
+REM set PYTHONPATH=%BASE_TOOLS_PATH%\Source\Python
+REM call FreezePython script here!
+REM popd
+REM
 
 @REM
 @REM Done!!!
@@ -104,7 +100,7 @@ goto end
 
 :no_tools_path
 echo.
-echo !!!WARNING!!! No tools path found. Please set EDK_TOOLS_PATH.
+echo !!! ERROR !!! No tools path found. Please set EDK_TOOLS_PATH.
 echo.
 goto end
 
@@ -117,4 +113,3 @@ echo.
 
 :end
 @echo on
-
