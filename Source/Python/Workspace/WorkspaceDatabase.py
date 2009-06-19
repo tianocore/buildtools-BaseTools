9c01e32ae4e36d72eb84f33cf656a1556ebe1853
klu2 <klu2@7335b38e-4728-0410-8992-fb3ffe349368>
2009-06-19 15:48:45 +0000

    Add additional checking for whether need renew the immediate file workspace database, the renew reason maybe: The renew reason maybe: 1) If user force to renew; 2) If user do not force renew, and     a) If the time of last modified python source is newer than database file;     b) If the time of last modified frozen executable file is newer than database file, the executable file is build.exe in window and build in Linux/Unix/OSX
    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@1628 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/Python/Workspace/WorkspaceDatabase.py
+++ Source/Python/Workspace/WorkspaceDatabase.py
@@ -2047,9 +2047,11 @@ class WorkspaceDatabase(object):
             DbDir = os.path.split(DbPath)[0]
             if not os.path.exists(DbDir):
                 os.makedirs(DbDir)
+                
         # remove db file in case inconsistency between db and file in file system
-        if RenewDb and os.path.exists(DbPath):
+        if self._CheckWhetherDbNeedRenew(RenewDb, DbPath):
             os.remove(DbPath)
+        
         # create db with optimized parameters
         self.Conn = sqlite3.connect(DbPath, isolation_level='DEFERRED')
         self.Conn.execute("PRAGMA synchronous=OFF")
@@ -2070,6 +2072,60 @@ class WorkspaceDatabase(object):
         self.BuildObject = WorkspaceDatabase.BuildObjectFactory(self)
         self.TransformObject = WorkspaceDatabase.TransformObjectFactory(self)
 
+    ## Check whether workspace database need to be renew.
+    #  The renew reason maybe:
+    #  1) If user force to renew;
+    #  2) If user do not force renew, and
+    #     a) If the time of last modified python source is newer than database file;
+    #     b) If the time of last modified frozen executable file is newer than database file;
+    #
+    #  @param force     User force renew database
+    #  @param DbPath    The absolute path of workspace database file
+    #
+    #  @return Bool value for whether need renew workspace databse
+    #
+    def _CheckWhetherDbNeedRenew (self, force, DbPath):
+        # if database does not exist, we need do nothing
+        if not os.path.exists(DbPath): return False
+            
+        # if user force to renew database, then not check whether database is out of date
+        if force: return True
+        
+        #    
+        # Check the time of last modified source file or build.exe
+        # if is newer than time of database, then database need to be re-created.
+        #
+        timeOfToolModified = 0
+        if hasattr(sys, "frozen"):
+            exePath             = os.path.abspath(sys.executable)
+            timeOfToolModified  = os.stat(exePath).st_mtime
+        else:
+            curPath  = os.path.dirname(__file__) # curPath is the path of WorkspaceDatabase.py
+            rootPath = os.path.split(curPath)[0] # rootPath is root path of python source, such as /BaseTools/Source/Python
+            if rootPath == "" or rootPath == None:
+                EdkLogger.verbose("\nFail to find the root path of build.exe or python sources, so can not \
+determine whether database file is out of date!\n")
+        
+            # walk the root path of source or build's binary to get the time last modified.
+        
+            for root, dirs, files in os.walk (rootPath):
+                for dir in dirs:
+                    # bypass source control folder 
+                    if dir.lower() in [".svn", "_svn", "cvs"]:
+                        dirs.remove(dir)
+                        
+                for file in files:
+                    ext = os.path.splitext(file)[1]
+                    if ext.lower() == ".py":            # only check .py files
+                        fd = os.stat(os.path.join(root, file))
+                        if timeOfToolModified < fd.st_mtime:
+                            timeOfToolModified = fd.st_mtime
+        if timeOfToolModified > os.stat(DbPath).st_mtime:
+            EdkLogger.verbose("\nWorkspace database is out of data!")
+            return True
+            
+        return False
+            
     ## Initialize build database
     def InitDatabase(self):
         EdkLogger.verbose("\nInitialize build database started ...")
