e41b159a4adb9acce451fc205f4dd6146f755d8f
qhuang8 <qhuang8@c2973edb-eda0-4c78-bc6a-9341b269661f>
2010-02-12 08:28:55 +0000

    Add @REM for windows BAT file comment
    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@1872 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/C/Makefiles/NmakeSubdirs.bat
+++ Source/C/Makefiles/NmakeSubdirs.bat
@@ -1,13 +1,14 @@
-## @file
-#
-# Copyright (c) 2007 - 2010, Intel Corporation
-# All rights reserved. This program and the accompanying materials
-# are licensed and made available under the terms and conditions of the BSD License
-# which accompanies this distribution.    The full text of the license may be found at
-# http://opensource.org/licenses/bsd-license.php
-#
-# THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-# WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+@REM ## @file
+@REM #
+@REM # Copyright (c) 2007 - 2010, Intel Corporation
+@REM # All rights reserved. This program and the accompanying materials
+@REM # are licensed and made available under the terms and conditions of the BSD License
+@REM # which accompanies this distribution.    The full text of the license may be found at
+@REM # http://opensource.org/licenses/bsd-license.php
+@REM #
+@REM # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+@REM # WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
 
 @echo off
 setlocal
