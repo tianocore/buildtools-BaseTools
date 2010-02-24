4a9de6567568a97012bc0ca73cfddb375209266c
qhuang8 <qhuang8@c2973edb-eda0-4c78-bc6a-9341b269661f>
2010-02-24 03:27:13 +0000

    Update the build tools devel mail list in python source.
    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@1891 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/Python/build/build.py
+++ Source/Python/build/build.py
@@ -1842,7 +1842,7 @@ def Main():
                     "\nbuild",
                     CODE_ERROR,
                     "Unknown fatal error when processing [%s]" % MetaFile,
-                    ExtraData="\n(Please send email to dev@buildtools.tianocore.org for help, attaching following call stack trace!)\n",
+                    ExtraData="\n(Please send email to edk2-buildtools-devel@lists.sourceforge.net for help, attaching following call stack trace!)\n",
                     RaiseError=False
                     )
         EdkLogger.quiet("(Python %s on %s) " % (platform.python_version(), sys.platform) + traceback.format_exc())
