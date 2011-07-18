6360e17bfe143991da4a76862f5b1acd0dd8360f
gikidy <gikidy@c2973edb-eda0-4c78-bc6a-9341b269661f>
2011-07-18 02:20:35 +0000

    Add FvBaseAddress attribute in [FV] section. Signed-off-by: gikidy Reviewed-by: lhauch Reviewed-by: lgao4
    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@2222 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/Python/GenFds/Fv.py
+++ Source/Python/GenFds/Fv.py
@@ -46,7 +46,8 @@ class FV (FvClassObject):
         self.InfFileName = None
         self.FvAddressFileName = None
         self.CapsuleName = None
-
+        self.FvBaseAddress = None
+        
     ## AddToBuffer()
     #
     #   Generate Fv and add it to the Buffer
@@ -81,9 +82,12 @@ class FV (FvClassObject):
                             elif RegionData.upper() + 'fv' in GenFds.ImageBinDict.keys():
                                 continue
                             elif self.UiFvName.upper() == RegionData.upper():
-                               GenFdsGlobalVariable.ErrorLogger("Capsule %s in FD region can't contain a FV %s in FD region." % (self.CapsuleName, self.UiFvName.upper()))
+                                GenFdsGlobalVariable.ErrorLogger("Capsule %s in FD region can't contain a FV %s in FD region." % (self.CapsuleName, self.UiFvName.upper()))
 
         GenFdsGlobalVariable.InfLogger( "\nGenerating %s FV" %self.UiFvName)
+        
+        if self.FvBaseAddress != None:
+            BaseAddress = self.FvBaseAddress
 
         self.__InitializeInf__(BaseAddress, BlockSize, BlockNum, ErasePloarity, VtfDict)
         #
