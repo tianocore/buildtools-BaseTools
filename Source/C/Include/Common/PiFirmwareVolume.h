15de8895cbd333319fb5236fd89f29e8cf0a34c2
ydong10 <ydong10@c2973edb-eda0-4c78-bc6a-9341b269661f>
2011-11-16 06:26:09 +0000

    According to PI 1.2c Vol 3, EFI_FVB2_ALIGNMNET_512K should be EFI_FVB2_ALIGNMENT_512K.
    Signed-off-by: lzeng14
    Reviewed-by: lgao4
    
    git-svn-id: https://edk2-buildtools.svn.sourceforge.net/svnroot/edk2-buildtools/trunk/BaseTools@2409 c2973edb-eda0-4c78-bc6a-9341b269661f

--- Source/C/Include/Common/PiFirmwareVolume.h
+++ Source/C/Include/Common/PiFirmwareVolume.h
@@ -1,7 +1,7 @@
 /** @file
   The firmware volume related definitions in PI.
 
-  Copyright (c) 2006 - 2008, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
 
   This program and the accompanying materials are licensed and made available
   under the terms and conditions of the BSD License which accompanies this
@@ -73,7 +73,7 @@ typedef UINT32  EFI_FVB_ATTRIBUTES;
 #define EFI_FVB2_ALIGNMENT_64K      0x00100000
 #define EFI_FVB2_ALIGNMENT_128K     0x00110000
 #define EFI_FVB2_ALIGNMENT_256K     0x00120000
-#define EFI_FVB2_ALIGNMNET_512K     0x00130000
+#define EFI_FVB2_ALIGNMENT_512K     0x00130000
 #define EFI_FVB2_ALIGNMENT_1M       0x00140000
 #define EFI_FVB2_ALIGNMENT_2M       0x00150000
 #define EFI_FVB2_ALIGNMENT_4M       0x00160000
